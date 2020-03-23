16a70f1246c159a5cd543196924d6d560734ec09
Misael Aguilar <misadina95@gmail.com>
2020-03-23 11:21:42 -0600

    Tabulator Bitacora

--- application/views/bitacora/bitacora_js.php
+++ application/views/bitacora/bitacora_js.php
@@ -10,20 +10,21 @@
 var table = new Tabulator("#tablas", {
     layout:"fitDataFill",
     data:<?=file_get_contents(base_url().'ejemplo.json');?>,
-
-    columns:[
+    autoColumns: true,
+   /*  columns:[
         
-    {title:"Id", field:"id", width:50, responsive:0}, //never hide this column
-    {title:"Id doc", field:"id_doc", width:120},
-    {title:"Documento", field:"nom_doc", width:150},
+    {title:"Id Bitacora", field:"id_bitacora", width:50, responsive:0}, //never hide this column
     {title:"Id Usuario", field:"id_usuario", width:120},
-    {title:"Usuario", field:"nom_usuario", width:150},
-    {title:"Id Evento", field:"id_evento", width:120},
-    {title:"Evento", field:"nom_evento", width:150},
-    {title:"Fecha Evt", field:"fecha_evt", width:130},
+    {title:"Usuario", field:"usuario", width:150},
+    {title:"Id Documento", field:"id_documento", width:120},
+    {title:"# Documento", field:"numero_documento", width:150},
+    {title:"Id Usuario mod", field:"id_usuario_modificado", width:120},
+    {title:"Usuario Modificado", field:"usuario_modificado", width:150},
+    {title:"Evento", field:"evento", width:130},
+    {title:"Fecha Evento", field:"fecha_evento", width:130},
 
 
-    ],
-});
+    ],*/
+}); 
 
 </script>

4db43ba1fb8266c3412837d9b0694b81d0eb5fe9
Misael Aguilar <misadina95@gmail.com>
2020-03-27 15:04:05 -0600

    seguridad

--- application/controllers/Inicio.php
+++ application/controllers/Inicio.php
@@ -10,16 +10,31 @@ class Inicio extends CI_Controller {
 	//Constructor
 	function __construct(){
 		parent::__construct();
-		$this->api = new RestClient([
-			'base_url' => "http://10.254.250.17:81/api",
-			'headers' => ['ephylone'=>'sas-doc']
-		]);
-	}
+		$this->load->helper(array('form', 'url'));
 
+		$this->api = new RestClient([
+			'base_url' => 'http://10.254.250.17/API_REST/api', 
+			'headers' => [
+				'Ephylone'=>'doc',
+				'Autorizacion' => $this->session->token]
+		]);
+		//$this->seguridad();
+		}
+		/* private function seguridad(){
+			if(isset($_SESSION['token'])){
+				if(!json_decode($this->api->GET('/validacion')->response)->status){
+					$this->salir();
+				}
+			}
+			else{
+				$this->salir();
+			}
+		} */
+		private function response($arr){
+			echo json_encode($arr);
+		}
 	public function index(){
 		// if(isset($this->session->data_info['token'])){
-		// 	$this->load->library('componentes');
-		// 	$this->principal();
 		// }else
 		// 	$this->login();
 		$this->load->library('componentes');
@@ -32,6 +47,11 @@ class Inicio extends CI_Controller {
 			$result .= '<option  >'.$obj[$i]->nombre.'</option>';
 		}
 		return $result;
+	}
+	public function salir(){
+		$this->session->sess_destroy();
+		header('Location: '.base_url().'login/');
+		exit;
 	}
 	
 	/* private function prepara($obj,$tipo=null){
@@ -141,6 +161,39 @@ class Inicio extends CI_Controller {
 		$this->api = new RestClient();
 		var_dump($this->api->post('http://127.0.0.1/api_rest/autorizacion/inicio', 'POST', $par));
 	}
+	public function insertar(){
+		if($_POST['documento'] != ''){
+			$_POST['usuario_id'] = $this->session->num_emp;
+			$_POST['fecha_creacion'] = date('Y-m-d');
+			$res = $this->api->post('/insertar',array('datos'=>$_POST,'tabla'=>'dbo.documentos'));
+			if($res['ban'])
+				$this->response(array('msg'=>true));
+			else
+				$this->response(array('msg'=>false,'error'=>$res['error']));
+		}
+		else{
+			$this->response(array('msg'=>false,'error'=>'El documento no puede ir vacio'));
+		}		
+	}
+
+	public function actualizar(){
+		if($_POST['documento'] != ''){
+			$res = $this->api->post('/actualizar',array('datos'=>array('documento'=>$_POST['documento']),'tabla'=>'dbo.documentos','condicion'=>array('id'=>$_POST['id'])));
+			if($res['ban'])
+				$this->response(array('msg'=>true));
+			else
+				$this->response(array('msg'=>false,'error'=>$res['error']));
+		}
+	}
+
+	public function eliminar(){
+		$res = $this->api->post('/eliminar',array('datos'=>$_POST,'tabla'=>'dbo.docuemntos'));
+		if($res['ban'])
+			$this->response(array('msg'=>true));
+		else
+			$this->response(array('msg'=>false,'error'=>$res['error']));
+	}
+
 
 	private function respuesta($data){
 		var_dump($data->response);
